d949250fd673f2a9220cd97b4563744be1c56c3c
andy fang <andy@mingtak.com.tw>
2015-03-23 14:39:23 +0000

    initial version

---
+++ funlog/portlet/youtube/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+YoutubeVideoMessageFactory = MessageFactory('funlog.portlet.youtube')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
